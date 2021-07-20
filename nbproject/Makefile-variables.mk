7bd6bcc63ec8b0da6ce347b0723aa2da94faf6bb
StefanyABL <73035293+StefanyABL@users.noreply.github.com>
2021-07-19 22:35:25 -0500

    Add files via upload

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=Cygwin-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin-Windows
+CND_ARTIFACT_NAME_Debug=ejerciciounolistas
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Windows/ejerciciounolistas
+CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin-Windows/package
+CND_PACKAGE_NAME_Debug=ejerciciounolistas.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Windows/package/ejerciciounolistas.tar
+# Release configuration
+CND_PLATFORM_Release=Cygwin-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/Cygwin-Windows
+CND_ARTIFACT_NAME_Release=ejerciciounolistas
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin-Windows/ejerciciounolistas
+CND_PACKAGE_DIR_Release=dist/Release/Cygwin-Windows/package
+CND_PACKAGE_NAME_Release=ejerciciounolistas.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin-Windows/package/ejerciciounolistas.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
